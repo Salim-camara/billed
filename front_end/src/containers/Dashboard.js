02a561c69c601e5472d24b28b1c4d2052424f9ce
Camara Mohamed Salim <m.salimcamara@gmail.com>
2022-11-03 14:54:42 +0100

    feat(Dashboard): show bill work correcctly

--- front_end/src/containers/Dashboard.js
+++ front_end/src/containers/Dashboard.js
@@ -86,6 +86,17 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
+    console.log('89 ', bill)
+    let state = null
+    switch (bill.status) {
+      case 'pending': state = 'Attentes';
+      break;
+      case 'accepted': state = 'Validations';
+      break;
+      case 'refused': state = 'Refus';
+      break;
+    }
+    this.document.querySelector('#statusOfBill').innerHTML = `${state}`
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
@@ -95,7 +106,7 @@ export default class {
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
+      // this.counter ++
     } else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
@@ -103,7 +114,7 @@ export default class {
         <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `)
       $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
+      // this.counter ++
     }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
