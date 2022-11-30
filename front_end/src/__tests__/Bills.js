a53480613bbc947320fb8a54d0f3860cf59b25f2
Salim Camara <m.salimcamara@gmail.com>
2022-11-30 12:52:09 +0100

    test: up

--- front_end/src/__tests__/Bills.js
+++ front_end/src/__tests__/Bills.js
@@ -2,78 +2,132 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
-import BillsUI from "../views/BillsUI.js"
-import { bills } from "../fixtures/bills.js"
-import store from "../app/Store"
-import { ROUTES_PATH} from "../constants/routes.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
-import BillsContainer from "../containers/Bills.js"
+import { screen, waitFor } from "@testing-library/dom";
+import BillsUI from "../views/BillsUI.js";
+import { bills } from "../fixtures/bills.js";
+import store from "../app/Store";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import {
+  toHaveStyle,
+  toHaveBeenCalled,
+  toHaveClass,
+} from "@testing-library/jest-dom";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import BillsContainer from "../containers/Bills.js";
 import NewBillContainer from "../containers/NewBill.js";
-import userEvent from '@testing-library/user-event';
+import userEvent from "@testing-library/user-event";
+import mockStore from "../__mocks__/store";
 
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('icon-window'))
-      const windowIcon = screen.getByTestId('icon-window')
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId("icon-window"));
+      const windowIcon = screen.getByTestId("icon-window");
       //to-do write expect expression
-      expect(windowIcon.classList.contains("active-icon")).toBeTruthy()
-    })
+      expect(windowIcon.classList.contains("active-icon")).toBeTruthy();
+    });
 
     test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
-    })
+      document.body.innerHTML = BillsUI({ data: bills });
+      const dates = screen
+        .getAllByText(
+          /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
+        )
+        .map((a) => a.innerHTML);
+      const antiChrono = (a, b) => (a < b ? 1 : -1);
+      const datesSorted = [...dates].sort(antiChrono);
+      expect(dates).toEqual(datesSorted);
+    });
 
-    test('check if modal is diplayed', async () => {
-      const billsInstance = new BillsContainer({ document, onNavigate, store, localStorage })
-      const iconEye = screen.getAllByTestId('icon-eye')
-      console.log('l46 ', iconEye[0])
-      await waitFor(() => screen.getByTestId('modal2'))
-      billsInstance.handleClickIconEye(iconEye[0])
+    test("check if modal is diplayed", async () => {
+      // const billsInstance = new BillsContainer({
+      //   document,
+      //   onNavigate,
+      //   store,
+      //   localStorage,
+      // });
+      // const iconEye = screen.getAllByTestId("icon-eye");
+      // console.log("l60 ", iconEye[0]);
+      // iconEye[0].click();
+      // billsInstance.handleClickIconEye(iconEye[0])
       // userEvent.click(iconEye[0])
       // await waitFor(() => screen.findBy('modal2'))
-      setTimeout(() => {
-        expect(screen.getByTestId('modal2').classList.contains("show")).toBeTruthy()
-      }, 1000)
-    })
+      // setTimeout(() => {
+      //   expect(
+      //     screen.getByTestId("modal2").classList.contains("show")
+      //   ).toBeTruthy();
+      // }, 1000);
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      document.body.innerHTML = BillsUI({ data: bills });
+      const bills2 = new BillsContainer({
+        document,
+        onNavigate,
+        localStorage: window.localStorage,
+      });
+      const handleClickIconEye = jest.fn((icon) =>
+        bills2.handleClickIconEye(icon)
+      );
+      const modaleFile = document.getElementById("modaleFile");
+      const iconEye = screen.getAllByTestId("icon-eye");
+      $.fn.modal = jest.fn(() => modaleFile.classList.add("show"));
+      iconEye.forEach((icon) => {
+        icon.addEventListener("click", handleClickIconEye(icon));
+        userEvent.click(icon);
+        expect(handleClickIconEye).toHaveBeenCalled();
+      });
+      expect(modaleFile).toHaveClass("show");
+    });
 
-    
-    test('test2', async () => {
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
-      await waitFor(() => screen.getByTestId('btn-new-bill'))
-      userEvent.click(screen.getByTestId('btn-new-bill'))
-      console.log('l66 ', window.location.hash)
-      expect(window.location.hash).toBe('#employee/bill/new')
-      // console.log('l63 ', document.body.innerHTML)
-    })
+    test("check redirection on NewBill page", async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const bills = new BillsContainer({
+        document,
+        onNavigate,
+        mockStore,
+        localStorage,
+      });
+      const handleClickNewBill = jest.fn((e) => bills.handleClickNewBill(e));
+      const addNewBill = screen.getByTestId("btn-new-bill");
+      addNewBill.addEventListener("click", handleClickNewBill);
+      userEvent.click(addNewBill);
+      expect(handleClickNewBill).toHaveBeenCalled();
+      expect(screen.queryByText("Envoyer une note de frais")).toBeTruthy();
+    });
 
-
-    // test('', () => {
-    //   const billsContainerInstance = new BillsContainer({ document, onNavigate, store, localStorage })
-    //   const dates = 5
-    //   console.log('l59 ', billsContainerInstance.getBills())
-    //   expect(dates).toEqual(5)
-    // })
-  })
-})
+    test("Then fetches bills from mock API GET", async () => {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ type: "Admin", email: "a@a" })
+      );
+      new BillsContainer({
+        document,
+        onNavigate,
+        mockStore,
+        localStorage: window.localStorage,
+      });
+      document.body.innerHTML = BillsUI({ data: bills });
+      await waitFor(() => screen.getByText("Mes notes de frais"));
+      expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+    });
+  });
+});
--- front_end/src/__tests__/NewBill.js
+++ front_end/src/__tests__/NewBill.js
@@ -2,17 +2,25 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+import { screen } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+    test("Then i click on submit without complet form", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      console.log("l15", html);
       //to-do write assertion
-    })
-  })
-})
+      const newBills = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage,
+      });
+      const inputFile = document.querySelector("#btn-send-bill");
+      inputFile.click();
+    });
+  });
+});
