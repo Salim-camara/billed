449398e3c8f0e18b9e1ca19ffed77324425824ba
Camara Mohamed Salim <m.salimcamara@gmail.com>
2023-05-01 01:35:02 +0200

    up

--- front_end/src/__tests__/NewBill.js
+++ front_end/src/__tests__/NewBill.js
@@ -5,6 +5,7 @@
 import { screen, waitFor, fireEvent, toHaveClass } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
+import BillsUI from "../views/BillsUI.js";
 import userEvent from "@testing-library/user-event";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store";
@@ -30,7 +31,7 @@ describe("Given I am connected as an employee", () => {
           localStorage: window.localStorage,
         });
         localStorage.setItem(
-          "user", 
+          "user",
           JSON.stringify({
             type: "Employee",
             email: "test@test.fr",
@@ -44,10 +45,56 @@ describe("Given I am connected as an employee", () => {
         expect(handleSubmit).toHaveBeenCalled();
         await waitFor(() => screen.getByTestId("btn-new-bill"));
       });
+
+      // test post
+      describe("When I submit a new Bill on correct format", () => {
+        test("Then the submit should success", () => {
+          const html = NewBillUI();
+          document.body.innerHTML = html;
+          const newBill1 = new NewBill({
+            document,
+            onNavigate,
+            store,
+            localStorage: window.localStorage,
+          });
+          const formNewBill = screen.getByTestId("form-new-bill");
+          expect(formNewBill).toBeTruthy();
+          const handleSubmit = jest.fn((e) => newBill1.handleSubmit(e));
+          formNewBill.addEventListener("submit", handleSubmit);
+          fireEvent.submit(formNewBill);
+          expect(handleSubmit).toHaveBeenCalled();
+        });
+        describe("When an error occurs", () => {
+          test("should fail with 500 message error", async () => {
+            jest.spyOn(mockStore, "bills");
+            Object.defineProperty(window, "localStorage", {
+              value: localStorageMock,
+            });
+            window.localStorage.setItem(
+              "user",
+              JSON.stringify({
+                type: "Employee",
+                email: "a@a",
+              })
+            );
+            mockStore.bills.mockImplementationOnce(() => {
+              return {
+                create: () => {
+                  return Promise.reject(new Error("Erreur 500"));
+                },
+              };
+            });
+            const html = BillsUI({ error: "Erreur 500" });
+            document.body.innerHTML = html;
+            const message = await screen.getByText(/Erreur 500/);
+            expect(message).toBeTruthy();
+          });
+        });
+      });
     });
     test("Then i change file", () => {
       const html = NewBillUI();
-        document.body.innerHTML = html;
+      document.body.innerHTML = html;
       window.localStorage.setItem(
         "user",
         JSON.stringify({
